18ae5a47e9d6d96dbf499816566307f8e70a1561
selma <selma04k@gmail.com>
2025-05-15 12:09:25 +0200

    baza

---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "Healthcare";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+
+if ($conn->connect_error) {
+    die("Konekcija nije uspjela: " . $conn->connect_error);
+}
+
+// Ako je konekcija uspješna
+// echo "Uspješno povezano s bazom podataka!";
+?>
